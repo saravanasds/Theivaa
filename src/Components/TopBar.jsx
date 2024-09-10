c8bc967d7e4338155bfeb8957fa655a47a0ebdc8
Aananthi1234 <anuskpk1208@gmail.com>
2024-09-10 13:58:07 +0530

    one

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -6,20 +6,20 @@ import { FaPhone } from "react-icons/fa";
 const TopBar = () => {
   return (
     <div className="w-full h-auto sticky top-0 bg-primary text-white font-medium lg:py-3 py-2 px-5 text-lg tracking-wider">
-      <div className="flex justify-between items-center">
+      <div className="flex  justify-between items-center ">
         <div className="flex items-center gap-2 text-secondary">
           <IoMdMail className='lg:text-lg text-sm' />
-          <a href="#" className="text-white tracking-[0.05em] lg:text-base text-xs">technoschoolpalani@gmail.com</a>
+          <a href="#" className="text-white tracking-[0.05em] lg:text-base text-[10px]">technoschoolpalani@gmail.com</a>
         </div>
 
         <div className="flex items-center gap-5 text-secondary">
-          <small className="flex items-center gap-2">
-            <MdLocationPin className='lg:text-lg' />
+          <small className=" items-center gap-2 lg:block hidden">
+            <MdLocationPin className='lg:text-lg text-[15px]' />
             <a href="#" className="text-white text-sm lg:text-base">Palani-624601</a>
           </small>
           <small className="flex items-center gap-2">
-            <FaPhone className='lg:text-lg' />
-            <a href="#" className="text-white tracking-[0.05em] lg:text-base text-xs">9994500216</a>
+            <FaPhone className='lg:text-lg text-[15px]' />
+            <a href="#" className="text-white tracking-[0.05em] lg:text-base text-[10px]">9994500216</a>
           </small>
         </div>
       </div>
